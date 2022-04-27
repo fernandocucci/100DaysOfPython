78397899dbff9040227e028c196f78fc4bfa4b64
Fernando <fernandocucci@gmail.com>
2022-04-27 22:30:32 +0900

    Day 002 - Finished

--- Day 002/main.py
+++ Day 002/main.py
@@ -1,3 +1,10 @@
 print('==========================================')
 print('| Day 002 - Project: Tip Calculator      |')
 print('==========================================')
+bill = float(input("How much is the bill?: $"))
+tip = int(input("Tip % (10,15,20)): "))
+people = int(input("Split the bill in how many persons?: "))
+
+total = "{:.2f}".format(bill*(1+(tip/100))/people)
+
+print(f"Each person must pay ${total}")

430c08564c0dbd347dbb87db1e2a70bbe6e40d1c
Fernando <fernandocucci@gmail.com>
2022-05-01 09:51:25 +0900

    Day 006 - Solution
    Day 006 - Solution

--- Day 006/main.py
+++ Day 006/main.py
@@ -1,3 +1,22 @@
 print('==========================================')
-print('| Day 006 - Project: ?                   |')
+print('| Day 006 - Project: Reeborgs Maze       |')
 print('==========================================')
+
+#Solution to Maze at https://reeborg.ca/
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if at_goal():
+        done()
+    elif right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()

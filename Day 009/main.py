f903b4f6ea1f5222a1bf725617e183f652a52951
Fernando <fernandocucci@gmail.com>
2022-05-04 23:47:16 +0900

    Day 009 - Finished
    Finished

--- Day 009/main.py
+++ Day 009/main.py
@@ -1,3 +1,28 @@
 #==========================================
 # Day 009 - Project: Secret Auction
 #==========================================
+
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+
+selection = ""
+offers = {}
+
+while selection != "no":
+    cls()
+    name = input("Type your Name: ")
+    offer = int(input("Type your Offer: $"))
+    offers[name] = offer
+    selection = input("Is there another person? (yes/no): ")
+
+cls()
+max_name = ""
+max_offer = 0
+for offer in offers:
+    if offers[offer] > max_offer:
+        max_offer = offers[offer]
+        max_name = offer
+print(f"The winner is {max_name} with a bid of ${max_offer}")
